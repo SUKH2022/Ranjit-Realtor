7f15968d78fc81544dac67d15602173c8b26e15e
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2024-09-09 09:34:48 -0400

    created pages with content and routes

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  };
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore()
